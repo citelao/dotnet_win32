bcb78fcd880662774320322def75f069707dfb96
Ben Stolovitz <ben@stolovitz.com>
2024-09-17 19:10:28 -0400

    show events

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.Accessibility;
+
 Console.WriteLine("Hello, World!");
+
+WINEVENTPROC HandleWinEvent = (hWinEventHook, ev, hwnd, idObject, idChild, dwEventThread, dwmsEventTime) =>
+{
+    Console.WriteLine($"Event: {ev}");
+};
+
+var hook = PInvoke.SetWinEventHook(
+    PInvoke.EVENT_SYSTEM_FOREGROUND,
+    PInvoke.EVENT_SYSTEM_FOREGROUND,
+    HMODULE.Null,
+    HandleWinEvent,
+    0, // all processes
+    0, // all threads
+    PInvoke.WINEVENT_OUTOFCONTEXT
+);
+
+// Start a message loop
+while (PInvoke.GetMessage(out var msg, HWND.Null, 0, 0))
+{
+    PInvoke.TranslateMessage(msg);
+    PInvoke.DispatchMessage(msg);
+}

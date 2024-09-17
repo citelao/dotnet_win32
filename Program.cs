ed50de853cea3157a8f8d9d62c7059e84f2a6d45
Ben Stolovitz <ben@stolovitz.com>
2024-09-17 19:22:30 -0400

    safehandle so we are using

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
-﻿using Windows.Win32;
+﻿using System.Runtime.InteropServices;
+using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.UI.Accessibility;
 
 WINEVENTPROC HandleWinEvent = (hWinEventHook, ev, hwnd, idObject, idChild, dwEventThread, dwmsEventTime) =>
 {
-    Console.WriteLine($"Event: {ev}");
+    Console.WriteLine($"Event: {ev} hwnd: {hwnd} idObject: {idObject} idChild: {idChild} dwEventThread: {dwEventThread} dwmsEventTime: {dwmsEventTime}");
 };
 
 Console.WriteLine("Registering event hook...");
-var hook = PInvoke.SetWinEventHook(
+using var hook = PInvoke.SetWinEventHook(
     PInvoke.EVENT_SYSTEM_FOREGROUND,
     PInvoke.EVENT_SYSTEM_FOREGROUND,
-    HMODULE.Null,
+    new NoReleaseSafeHandle(0),
     HandleWinEvent,
     0, // all processes
     0, // all threads
@@ -25,4 +26,21 @@ while (PInvoke.GetMessage(out var msg, HWND.Null, 0, 0))
 {
     PInvoke.TranslateMessage(msg);
     PInvoke.DispatchMessage(msg);
+}
+
+// https://github.com/microsoft/CsWin32/blob/99ddd314ea359d3a97afa82c735b6a25eb25ea32/test/WinRTInteropTest/Program.cs#L144
+class NoReleaseSafeHandle : SafeHandle
+{
+    public NoReleaseSafeHandle(int value)
+        : base(IntPtr.Zero, true)
+    {
+        this.SetHandle(new IntPtr(value));
+    }
+
+    public override bool IsInvalid => throw new NotImplementedException();
+
+    protected override bool ReleaseHandle()
+    {
+        return true;
+    }
 }

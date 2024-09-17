a48972b2fcc25e81fba9f6ec1ea746162947c57c
Ben Stolovitz <ben@stolovitz.com>
2024-09-17 19:13:18 -0400

    better comments

--- Program.cs
+++ Program.cs
@@ -2,13 +2,12 @@
 using Windows.Win32.Foundation;
 using Windows.Win32.UI.Accessibility;
 
-Console.WriteLine("Hello, World!");
-
 WINEVENTPROC HandleWinEvent = (hWinEventHook, ev, hwnd, idObject, idChild, dwEventThread, dwmsEventTime) =>
 {
     Console.WriteLine($"Event: {ev}");
 };
 
+Console.WriteLine("Registering event hook...");
 var hook = PInvoke.SetWinEventHook(
     PInvoke.EVENT_SYSTEM_FOREGROUND,
     PInvoke.EVENT_SYSTEM_FOREGROUND,
@@ -20,6 +19,8 @@ var hook = PInvoke.SetWinEventHook(
 );
 
 // Start a message loop
+// https://stackoverflow.com/a/12931599/788168
+Console.WriteLine("Starting message loop...");
 while (PInvoke.GetMessage(out var msg, HWND.Null, 0, 0))
 {
     PInvoke.TranslateMessage(msg);

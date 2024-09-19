475c460cf2f99f84d2eed5a56240b588f7d13fcd
Ben Stolovitz <ben@stolovitz.com>
2024-09-19 19:02:41 -0400

    base

---
+++ simple_systray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+
+
+
+
+// ----
+
+
+Console.WriteLine("Starting message loop...");
+Console.WriteLine("Press Ctrl-C to exit.");
+
+
+while (PInvoke.GetMessage(out var msg, HWND.Null, 0, 0))
+{
+    PInvoke.TranslateMessage(msg);
+    PInvoke.DispatchMessage(msg);
+}
+
+// Adapted directly from:
+// https://github.com/microsoft/CsWin32/blob/99ddd314ea359d3a97afa82c735b6a25eb25ea32/test/WinRTInteropTest/Program.cs#L144
+class NoReleaseSafeHandle : SafeHandle
+{
+    public NoReleaseSafeHandle(int value)
+        : base(IntPtr.Zero, true)
+    {
+        this.SetHandle(new IntPtr(value));
+    }
+
+    public override bool IsInvalid => throw new NotImplementedException();
+
+    protected override bool ReleaseHandle()
+    {
+        return true;
+    }
+}

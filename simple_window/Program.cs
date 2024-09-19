8d5c0919f372caa162b41d88775aa1ed6382bdb3
Ben Stolovitz <ben@stolovitz.com>
2024-09-19 18:17:59 -0400

    add simple window stub

---
+++ simple_window/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+// using Windows.Win32.UI.Accessibility;
+
+// Start a message loop to pump messages, or the hook won't be called.
+// https://stackoverflow.com/a/12931599/788168
+Console.WriteLine("Starting message loop...");
+Console.WriteLine("Press Ctrl-C to exit.");
+while (PInvoke.GetMessage(out var msg, HWND.Null, 0, 0))
+{
+    PInvoke.TranslateMessage(msg);
+    PInvoke.DispatchMessage(msg);
+}
+
+// Adapted directly from:
+// https://github.com/microsoft/CsWin32/blob/99ddd314ea359d3a97afa82c735b6a25eb25ea32/test/WinRTInteropTest/Program.cs#L144
+class NoReleaseSafeHandle : SafeHandle
+{
+    public NoReleaseSafeHandle(int value)
+        : base(IntPtr.Zero, true)
+    {
+        this.SetHandle(new IntPtr(value));
+    }
+
+    public override bool IsInvalid => throw new NotImplementedException();
+
+    protected override bool ReleaseHandle()
+    {
+        return true;
+    }
+}

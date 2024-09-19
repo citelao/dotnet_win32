acd93e434212cda050a67b1160765108b267a782
Ben Stolovitz <ben@stolovitz.com>
2024-09-19 18:49:42 -0400

    works

--- simple_window/Program.cs
+++ simple_window/Program.cs
@@ -2,13 +2,49 @@
 using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.UI.WindowsAndMessaging;
+using Windows.Win32.Graphics.Gdi;
+
+// Heavily inspired by https://github.com/microsoft/CsWin32/blob/99ddd314ea359d3a97afa82c735b6a25eb25ea32/test/WinRTInteropTest/Program.cs
+
+const string WindowClassName = "SimpleWindow";
+
+// Use the variable style function here to get easy access to the full function
+// signature. This function isn't used directly.
+// WNDPROC intermediateWndProc = (hwnd, msg, wParam, lParam) =>
+// {
+//     return WndProc(hwnd, msg, wParam, lParam);
+// };
+
+unsafe
+{
+    fixed (char* pClassName = WindowClassName)
+    {
+        var wndClass = new WNDCLASSEXW
+        {
+            // cbClsExtra = 0,
+            cbSize = (uint)Marshal.SizeOf<WNDCLASSEXW>(),
+            // cbWndExtra = 0,
+            hbrBackground = new HBRUSH((nint)SYS_COLOR_INDEX.COLOR_WINDOW + 1),
+            // hCursor = PInvoke.LoadCursorW(HINSTANCE.Null, IDC_ARROW),
+            // hIcon = PInvoke.LoadIconW(HINSTANCE.Null, IDI_APPLICATION),
+            // hIconSm = PInvoke.LoadIconW(HINSTANCE.Null, IDI_APPLICATION),
+            hInstance = PInvoke.GetModuleHandle(default(PCWSTR)),
+            lpfnWndProc = WndProc,
+            lpszClassName = pClassName,
+        };
+
+        // We ignore the returned class atom & use the class name directly.
+        // https://devblogs.microsoft.com/oldnewthing/20080501-00/?p=22503
+        PInvoke.RegisterClassEx(wndClass);
+    }
+}
 
 HWND hwnd;
 unsafe
 {
     hwnd = PInvoke.CreateWindowEx(
         0, // dwExStyle
-        "STATIC", // lpClassName
+        WindowClassName,
         "Hello, Windows!", // lpWindowName
         WINDOW_STYLE.WS_OVERLAPPEDWINDOW, // dwStyle
         0, // x
@@ -29,10 +65,16 @@ PInvoke.UpdateWindow(hwnd);
 // https://stackoverflow.com/a/12931599/788168
 Console.WriteLine("Starting message loop...");
 Console.WriteLine("Press Ctrl-C to exit.");
+
 while (PInvoke.GetMessage(out var msg, HWND.Null, 0, 0))
 {
     PInvoke.TranslateMessage(msg);
     PInvoke.DispatchMessage(msg);
+}
+
+static LRESULT WndProc(HWND hwnd, uint msg, WPARAM wParam, LPARAM lParam)
+{
+    return PInvoke.DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
 // Adapted directly from:

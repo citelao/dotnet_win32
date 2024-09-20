93c20fdc6665a52b3f8cd90b3920f637d4cc31be
Ben Stolovitz <ben@stolovitz.com>
2024-09-19 20:16:11 -0400

    robust mode

---
+++ robust_systray/Program.cs
@@ -0,0 +1,165 @@
+﻿using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.Shell;
+using Windows.Win32.UI.WindowsAndMessaging;
+using Windows.Win32.Graphics.Gdi;
+
+
+// Heavily inspired by https://github.com/microsoft/CsWin32/blob/99ddd314ea359d3a97afa82c735b6a25eb25ea32/test/WinRTInteropTest/Program.cs
+
+const string WindowClassName = "SimpleSystrayWindow";
+
+// Use the variable style function here to get easy access to the full function
+// signature. This function isn't used directly.
+// WNDPROC intermediateWndProc = (hwnd, msg, wParam, lParam) =>
+// {
+//     return WndProc(hwnd, msg, wParam, lParam);
+// };
+
+unsafe
+{
+    fixed (char* pClassName = WindowClassName)
+    {
+        var wndClass = new WNDCLASSEXW
+        {
+            // You need to include the size of this struct.
+            cbSize = (uint)Marshal.SizeOf<WNDCLASSEXW>(),
+
+            // Seems to work without this, but it seems sketchy to leave it out.
+            hInstance = PInvoke.GetModuleHandle(default(PCWSTR)),
+
+            // Required to actually run your WndProc (and the app will crash if
+            // null).
+            lpfnWndProc = WndProc,
+
+            // Required to identify the window class in CreateWindowEx.
+            lpszClassName = pClassName,
+        };
+
+        // We ignore the returned class atom & use the class name directly.
+        // https://devblogs.microsoft.com/oldnewthing/20080501-00/?p=22503
+        PInvoke.RegisterClassEx(wndClass);
+    }
+}
+
+HWND hwnd;
+unsafe
+{
+    hwnd = PInvoke.CreateWindowEx(
+        0, // dwExStyle
+        WindowClassName,
+        "Hello, Windows!", // lpWindowName
+        0, // dwStyle
+        0, // x
+        0, // y
+        640, // nWidth
+        480, // nHeight
+        HWND.Null, // hWndParent
+        new NoReleaseSafeHandle(0), // hMenu
+        new NoReleaseSafeHandle(0), // hInstance
+        null // lpParam
+    );
+}
+
+var guid = Guid.Parse("bc540dbe-f04e-4c1c-a5a0-01b32095b04c");
+
+// https://github.com/microsoft/WindowsAppSDK/discussions/519
+// https://github.com/microsoft/WindowsAppSDK/issues/713
+// https://github.com/File-New-Project/EarTrumpet/blob/bd42f1e235386c35c0989df8f2af8aba951f1848/EarTrumpet/UI/Helpers/ShellNotifyIcon.cs#L18
+// https://learn.microsoft.com/en-us/windows/win32/shell/notification-area
+// https://learn.microsoft.com/en-us/windows/win32/api/shellapi/ns-shellapi-notifyicondataa
+// https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-shell_notifyicona
+var notificationIconData = new NOTIFYICONDATAW
+{
+    // Required. You need to include the size of this struct.
+    cbSize = (uint)Marshal.SizeOf<NOTIFYICONDATAW>(),
+
+    // Required. An HWND is required to register the icon with the system.
+    // Window messages go there.
+    hWnd = hwnd,
+
+    // Required. Indicates which of the other members contain valid data.
+    // NIF_TIP and NIF_SHOWTIP are only required if you want to use szTip.
+    uFlags = NOTIFY_ICON_DATA_FLAGS.NIF_ICON | NOTIFY_ICON_DATA_FLAGS.NIF_GUID | NOTIFY_ICON_DATA_FLAGS.NIF_TIP | NOTIFY_ICON_DATA_FLAGS.NIF_SHOWTIP,
+
+    // TODO
+    uCallbackMessage = 0,
+
+    // Required. The icon to display.
+    // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-loadicona
+    // https://learn.microsoft.com/en-us/windows/win32/menurc/about-icons
+    hIcon = PInvoke.LoadIcon(HINSTANCE.Null, PInvoke.IDI_APPLICATION),
+
+    // Optional. You probably want a tooltip for your icon, though.
+    szTip = "Simple Systray",
+
+    // Required. A GUID to identify the icon. This should be persistent across
+    // launches and unique to your app!
+    guidItem = guid,
+
+    Anonymous = new()
+    {
+        // Recommended. VERSION_4 has been present since Vista and gives your
+        // app much richer window messages & more control over the icon tooltip.
+        //
+        // https://stackoverflow.com/q/41649303/788168
+        uVersion = PInvoke.NOTIFYICON_VERSION_4
+    }
+};
+if (!PInvoke.Shell_NotifyIcon(NOTIFY_ICON_MESSAGE.NIM_ADD, notificationIconData))
+{
+    Console.Error.WriteLine("Failed to add icon to the notification area.");
+    return;
+}
+if(!PInvoke.Shell_NotifyIcon(NOTIFY_ICON_MESSAGE.NIM_SETVERSION, notificationIconData))
+{
+    Console.Error.WriteLine("Failed to set version of icon in the notification area.");
+    return;
+}
+
+Console.WriteLine("Starting message loop...");
+Console.WriteLine("Press Ctrl-C to exit.");
+
+while (PInvoke.GetMessage(out var msg, HWND.Null, 0, 0))
+{
+    PInvoke.TranslateMessage(msg);
+    PInvoke.DispatchMessage(msg);
+}
+
+static LRESULT WndProc(HWND hwnd, uint msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+        case PInvoke.WM_CLOSE:
+            PInvoke.DestroyWindow(hwnd);
+            break;
+
+        case PInvoke.WM_DESTROY:
+            PInvoke.PostQuitMessage(0);
+            break;
+        
+        default:
+            return PInvoke.DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+
+    return new LRESULT(0);
+}
+
+// Adapted directly from:
+// https://github.com/microsoft/CsWin32/blob/99ddd314ea359d3a97afa82c735b6a25eb25ea32/test/WinRTInteropTest/Program.cs#L144
+class NoReleaseSafeHandle : SafeHandle
+{
+    public NoReleaseSafeHandle(int value)
+        : base(IntPtr.Zero, true)
+    {
+        this.SetHandle(new IntPtr(value));
+    }
+
+    public override bool IsInvalid => throw new NotImplementedException();
+
+    protected override bool ReleaseHandle()
+    {
+        return true;
+    }
+}
---
+++ robust_systray/README.md
@@ -0,0 +1,10 @@
+# ROBUST .NET systray icon
+
+This example app draws a more featureful systray icon.
+
+## Build & run
+
+```pwsh
+dotnet run
+# ctrl-c to exit
+```

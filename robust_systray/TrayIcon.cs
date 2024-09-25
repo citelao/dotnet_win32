f764c1fe315f5ab392113fee1105314b29f48890
Ben Stolovitz <ben@stolovitz.com>
2024-09-25 17:31:51 +0300

    extract a more generic message builder...

--- robust_systray/TrayIcon.cs
+++ robust_systray/TrayIcon.cs
@@ -19,49 +19,13 @@ internal class TrayIcon
         Guid = guid;
         OwnerHwnd = ownerHwnd;
 
-        // https://github.com/microsoft/WindowsAppSDK/discussions/519
-        // https://github.com/microsoft/WindowsAppSDK/issues/713
-        // https://github.com/File-New-Project/EarTrumpet/blob/bd42f1e235386c35c0989df8f2af8aba951f1848/EarTrumpet/UI/Helpers/ShellNotifyIcon.cs#L18
-        // https://learn.microsoft.com/en-us/windows/win32/shell/notification-area
-        // https://learn.microsoft.com/en-us/windows/win32/api/shellapi/ns-shellapi-notifyicondataa
-        // https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-shell_notifyicona
-        var notificationIconData = new NOTIFYICONDATAW
+        var notificationIconData = (new TrayIconMessageBuilder()
         {
-            // Required. You need to include the size of this struct.
-            cbSize = (uint)Marshal.SizeOf<NOTIFYICONDATAW>(),
-
-            // Required. An HWND is required to register the icon with the system.
-            // Window messages go there.
-            hWnd = ownerHwnd,
-
-            // Required. Indicates which of the other members contain valid data.
-            // NIF_TIP and NIF_SHOWTIP are only required if you want to use szTip.
-            uFlags = NOTIFY_ICON_DATA_FLAGS.NIF_ICON | NOTIFY_ICON_DATA_FLAGS.NIF_GUID | NOTIFY_ICON_DATA_FLAGS.NIF_TIP | NOTIFY_ICON_DATA_FLAGS.NIF_SHOWTIP,
-
-            // TODO
-            uCallbackMessage = 0,
-
-            // Required. The icon to display.
-            // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-loadicona
-            // https://learn.microsoft.com/en-us/windows/win32/menurc/about-icons
-            hIcon = PInvoke.LoadIcon(HINSTANCE.Null, PInvoke.IDI_APPLICATION),
-
-            // Optional. You probably want a tooltip for your icon, though.
-            szTip = _tooltip,
-
-            // Required. A GUID to identify the icon. This should be persistent across
-            // launches and unique to your app!
-            guidItem = guid,
-
-            Anonymous = new()
-            {
-                // Recommended. VERSION_4 has been present since Vista and gives your
-                // app much richer window messages & more control over the icon tooltip.
-                //
-                // https://stackoverflow.com/q/41649303/788168
-                uVersion = PInvoke.NOTIFYICON_VERSION_4
-            }
-        };
+            HWND = ownerHwnd,
+            Guid = guid,
+            Tooltip = _tooltip,
+            Icon = PInvoke.LoadIcon(HINSTANCE.Null, PInvoke.IDI_APPLICATION)
+        }).Build();
         if (!PInvoke.Shell_NotifyIcon(NOTIFY_ICON_MESSAGE.NIM_ADD, notificationIconData))
         {
             throw new Exception("Failed to add icon to the notification area.");
---
+++ robust_systray/TrayIconMessageBuilder.cs
@@ -0,0 +1,76 @@
+using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.Shell;
+using Windows.Win32.UI.WindowsAndMessaging;
+
+// TODO: public
+internal class TrayIconMessageBuilder
+{
+    public Guid Guid;
+    public HWND HWND;
+    public string? Tooltip = null;
+    public HICON Icon;
+
+    public NOTIFYICONDATAW Build()
+    {
+        // Generate the flags for the notification.
+        //
+        // We always use GUID IDs for these tray icons, so specify by it
+        // default.
+        NOTIFY_ICON_DATA_FLAGS flags = NOTIFY_ICON_DATA_FLAGS.NIF_GUID;
+        if (Tooltip != null)
+        {
+            flags |= NOTIFY_ICON_DATA_FLAGS.NIF_TIP | NOTIFY_ICON_DATA_FLAGS.NIF_SHOWTIP;
+        }
+        if (Icon != HICON.Null)
+        {
+            // TODO: handle updates that don't change the icon
+            flags |= NOTIFY_ICON_DATA_FLAGS.NIF_ICON;
+        }
+
+        // https://github.com/microsoft/WindowsAppSDK/discussions/519
+        // https://github.com/microsoft/WindowsAppSDK/issues/713
+        // https://github.com/File-New-Project/EarTrumpet/blob/bd42f1e235386c35c0989df8f2af8aba951f1848/EarTrumpet/UI/Helpers/ShellNotifyIcon.cs#L18
+        // https://learn.microsoft.com/en-us/windows/win32/shell/notification-area
+        // https://learn.microsoft.com/en-us/windows/win32/api/shellapi/ns-shellapi-notifyicondataa
+        // https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-shell_notifyicona
+        return new NOTIFYICONDATAW()
+        {
+            // Required. You need to include the size of this struct.
+            cbSize = (uint)Marshal.SizeOf<NOTIFYICONDATAW>(),
+
+            // Required. An HWND is required to register the icon with the system.
+            // Window messages go there.
+            hWnd = HWND,
+
+            // Required. Indicates which of the other members contain valid data.
+            // NIF_TIP and NIF_SHOWTIP are only required if you want to use szTip.
+            uFlags = flags,
+
+            // TODO
+            uCallbackMessage = 0,
+
+            // Required. The icon to display.
+            // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-loadicona
+            // https://learn.microsoft.com/en-us/windows/win32/menurc/about-icons
+            hIcon = Icon,
+
+            // Optional. You probably want a tooltip for your icon, though.
+            szTip = Tooltip,
+
+            // Required. A GUID to identify the icon. This should be persistent across
+            // launches and unique to your app!
+            guidItem = Guid,
+
+            Anonymous = new()
+            {
+                // Recommended. VERSION_4 has been present since Vista and gives your
+                // app much richer window messages & more control over the icon tooltip.
+                //
+                // https://stackoverflow.com/q/41649303/788168
+                uVersion = PInvoke.NOTIFYICON_VERSION_4
+            }
+        };
+    }
+}
